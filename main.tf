47979efd2d60a35e6814591948505f3876d55899
Raghu K <raghuk.vit@gmail.com>
2022-09-16 08:16:26 +0530

    Update terraform-modules/tf-module-mutable-app/

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "ondemand" {
+  count         = var.INSTANCES["ONDEMAND"].instance_count
+  instance_type = var.INSTANCES["ONDEMAND"].instance_type
+  ami           = data.aws_ami.ami.image_id
+}
+
+resource "aws_spot_instance_request" "spot" {
+  count                = var.INSTANCES["SPOT"].instance_count
+  instance_type        = var.INSTANCES["SPOT"].instance_type
+  ami                  = data.aws_ami.ami.image_id
+  wait_for_fulfillment = true
+}
+
