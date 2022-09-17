8de8abb33076bbc172cac831e74954920de7608d
Raghu K <raghuk.vit@gmail.com>
2022-09-17 08:53:35 +0530

    Update terraform-modules/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -1,4 +1,4 @@
-resource "aws_lb_target_group_attachment" "frontend" {
+resource "aws_lb_target_group_attachment" "tg" {
   count            = length(local.ALL_INSTANCE_IDS)
   target_group_arn = data.terraform_remote_state.infra.outputs.public_tg_arn
   target_id        = local.ALL_INSTANCE_IDS[count.index]
