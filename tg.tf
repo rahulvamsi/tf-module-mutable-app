7cae6b688603cde1261ba6ba119623d4e21cb49e
Raghu K <raghuk.vit@gmail.com>
2022-09-17 08:53:23 +0530

    Update terraform-modules/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -1,5 +1,6 @@
 resource "aws_lb_target_group_attachment" "frontend" {
-  target_group_arn = aws_lb_target_group.test.arn
-  target_id        = aws_instance.test.id
+  count            = length(local.ALL_INSTANCE_IDS)
+  target_group_arn = data.terraform_remote_state.infra.outputs.public_tg_arn
+  target_id        = local.ALL_INSTANCE_IDS[count.index]
   port             = 80
 }
