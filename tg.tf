e4983ceb4fc44e563e999165411ec10360a97f3d
Raghu K <raghuk.vit@gmail.com>
2022-09-21 08:02:26 +0530

    Update terraform-modules/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -4,3 +4,20 @@ resource "aws_lb_target_group_attachment" "tg" {
   target_id        = local.ALL_INSTANCE_IDS[count.index]
   port             = 80
 }
+
+resource "aws_lb_target_group" "tg" {
+  name                 = "${var.COMPONENT}-${var.ENV}"
+  port                 = var.APP_PORT
+  protocol             = "HTTP"
+  vpc_id               = data.terraform_remote_state.infra.outputs.vpc_id
+  deregistration_delay = 0
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    timeout             = 4
+    port                = var.APP_PORT
+    unhealthy_threshold = 2
+  }
+}
+
